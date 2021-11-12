0e25b66c5f3e66c6b42fbebd056a0a85ea6f31d6
Carolina Castro <carolinacoliveira@outlook.com>
2021-11-12 10:46:46 -0300

    add media_share

--- atividades-livro/media_share/app.rb
+++ atividades-livro/media_share/app.rb
@@ -1,24 +1,50 @@
-class Clip
-    attr_reader :comments
-
-    def initialize
-        @comments = []
+module AcceptsComments
+    def comments
+        # fornece um valor padrão a @comments, ou o valor atual.
+        # não há necessidade de initialize.
+        @comments ||= []
     end 
-
     def add_comment(comment)
-        comments << comment
+        comments << comment 
     end 
+end 
 
+
+class Clip
+    # Video e Song herdarão isso.
     def play 
         puts "Playing #{object_id}..."
     end 
-
 end 
 
-class Video < Clip 
+class Video < Clip
+    # mistura os métodos "comments" e "add_comment".
+    include AcceptsComments
     attr_accessor :resolution 
 end 
 
-class Song < Clip  
+class Song < Clip
+    # mistura os métodos "comments" e "add_comment".
+    include AcceptsComments 
     attr_accessor :beats_per_minute 
+end
+
+class Photo
+    # mistura os métodos "comments" e "add_comment".
+    include AcceptsComments
+    def initialize
+        @format = 'JPEG'
+    end 
 end 
+
+video = Video.new
+video.add_comment("Cool slow motion effect!")
+video.add_comment("Weird ending.")
+
+song = Song.new
+song.add_comment("Awesome beat.")
+
+photo = Photo.new 
+photo.add_comment("Beautil colors.")
+
+p video.comments, song.comments, photo.comments 
