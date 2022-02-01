a9c23f8cbed2dc83d89e2dd372f46ac50571c541
Carolina Castro <carolinacoliveira@outlook.com>
2022-01-31 22:38:13 -0300

    new activities

--- book_ruby/movies/app.rb
+++ book_ruby/movies/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'movie'
+require 'yaml'
 
 get('/movies') do 
     @movies = []
@@ -15,3 +16,10 @@ end
 get('/movies/new') do 
     erb :new 
 end 
+
+post('/movies/create') do 
+    @movie = Movie.new 
+    @movie.title = params['title']
+    @movie.director = params['director']
+    @movie.year = params['year']
+end
---
+++ the_odin_project/isogram.rb
@@ -0,0 +1,17 @@
+# isograma? (palavra que não possui letras repetidas)
+def isogram?(string)
+    original_length = string.length
+    string_array = string.downcase.split('')
+    
+    p string_array
+    
+    unique_length = string_array.uniq.length
+    
+    p unique_length
+    
+    original_length == unique_length
+end
+
+isogram?("Odin")
+
+
