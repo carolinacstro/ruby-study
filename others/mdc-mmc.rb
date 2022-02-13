6c6443dd7ed2f660b38a9614f4b51aefaeccf4dd
Carolina Castro <carolinacoliveira@outlook.com>
2022-02-13 20:31:29 -0300

    add others

---
+++ others/mdc-mmc.rb
@@ -0,0 +1,23 @@
+# avalia se um numero é par
+puts 42.even?
+
+
+# avalia se o numero é impar
+puts 100.odd?
+
+
+# podemos escrever assim
+number = 20
+puts "olá mundo" if number.even?
+
+
+# maximo divisor comum (MDC)
+puts 72.gcd(40)
+
+
+# minimo multiplo comum (MMC)
+puts 3.lcm(7)
+
+
+# MDC e MMC em conjunto - retorna um array
+puts 12.gcdlcm(20)
